35ec4f875cc7e81a3b4fd089cd5a18e396b58f47
robs <mnnnaleudes@gmail.com>
2021-09-14 11:43:18 -0300

    convert sender to typescript

--- src/controllers/SenderController.ts
+++ src/controllers/SenderController.ts
@@ -1,8 +1,14 @@
 import {Request, Response} from 'express';
+import Sender from '@services/Sender';
 
 class SenderController {
 
     async send(req: Request, res: Response) {
+
+        let queue = req.body.queue;
+        let message = req.body.message;
+
+        const sendMessage = await Sender.create(queue,message);
 
         return res.status(200).send();
 
